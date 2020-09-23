300f28bc6f1bb184a93c19958190882b9bfee115
Vaishnavi <vaishnavibalaraj28@gmail.com>
2020-09-23 20:54:50 +0530

    Adding the class for storing the list of promotion types

---
+++ Stock_Keeping_Unit/SKU_Calculator/Promotion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKU_Calculator
+{
+    class Promotion
+    {
+        private List<string> promotion_types;
+        /// <summary>
+        /// Getting and setting of Promotion_types
+        /// </summary>
+        public List<string> Promotion_types
+        {
+            get
+            {
+                return promotion_types;
+            }
+            set
+            {
+                promotion_types = value;
+            }
+        }
+    }
+}
