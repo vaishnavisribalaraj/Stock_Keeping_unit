b6a3706533e9e060dc4b58f846b359998368f7f4
Vaishnavi <vaishnavibalaraj28@gmail.com>
2020-09-23 20:38:50 +0530

    Adding the promotion segregation model class for SKU_Calculator

---
+++ Stock_Keeping_Unit/SKU_Calculator/Promotion_segregation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKU_Calculator
+{
+   public class Promotion_segregation
+    {
+        private char main_SKUID;
+        public char Main_SKUID { get; set; }
+
+        private char? secondary_SKUID;
+        public char? Secondary_SKUID { get; set; }
+
+        private int numberOfUnits;
+        public int NumberOfUnits { get; set; }
+
+        private int price;
+        public int Price { get; set; }
+    }
+}
