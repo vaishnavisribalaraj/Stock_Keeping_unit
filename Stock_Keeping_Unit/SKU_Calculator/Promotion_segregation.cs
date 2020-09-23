e4b862a2eb8e8a2dad4138151cfe62bf13200ee5
Vaishnavi <vaishnavibalaraj28@gmail.com>
2020-09-23 20:48:47 +0530

    Adding the comments for documentation

--- Stock_Keeping_Unit/SKU_Calculator/Promotion_segregation.cs
+++ Stock_Keeping_Unit/SKU_Calculator/Promotion_segregation.cs
@@ -6,15 +6,24 @@ namespace SKU_Calculator
 {
    public class Promotion_segregation
     {
+        // Promotion segregation is mainly for extracting the promotion rule
+        /// <summary>
+        /// Storing the Main SKUID and secondary SKUID
+        /// </summary>
         private char main_SKUID;
         public char Main_SKUID { get; set; }
 
         private char? secondary_SKUID;
         public char? Secondary_SKUID { get; set; }
 
+        /// <summary>
+        /// Number of units given in the pattern
+        /// </summary>
         private int numberOfUnits;
         public int NumberOfUnits { get; set; }
-
+        /// <summary>
+        /// Price given in the pattern
+        /// </summary>
         private int price;
         public int Price { get; set; }
     }
