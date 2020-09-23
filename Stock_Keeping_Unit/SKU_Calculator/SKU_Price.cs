c75773acddfbacb37bf69a219e406de5c1830825
Vaishnavi <vaishnavibalaraj28@gmail.com>
2020-09-23 20:57:42 +0530

    Adding the model class for storing the price values

---
+++ Stock_Keeping_Unit/SKU_Calculator/SKU_Price.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKU_Calculator
+{
+    class SKU_Price
+    {
+        //Adding the Properties
+        private char sKU_ID;
+        private int price;
+        /// <summary>
+        /// Getting and Setting the SKU_ID
+        /// </summary>
+        public char SKU_ID
+        {
+            get { return sKU_ID; }
+            set { sKU_ID = value; }
+        }
+        /// <summary>
+        /// Getting and setting the price for particular SKUID
+        /// </summary>
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
