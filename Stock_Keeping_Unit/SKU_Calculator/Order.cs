af4f9fa4c5a8b8b4c958391a4837bf6b6c2693c1
Vaishnavi <vaishnavibalaraj28@gmail.com>
2020-09-23 20:52:20 +0530

    Adding the Order model class for storing the Order entities

---
+++ Stock_Keeping_Unit/SKU_Calculator/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKU_Calculator
+{
+    class Order
+    {
+        private char sKUID;
+        private int units;
+        /// <summary>
+        /// Getting and Setting the SKUID
+        /// </summary>
+        public char SKUID
+        {
+            get { return sKUID; }
+            set { sKUID = value; }
+        }
+        /// <summary>
+        /// Getting and setting the units for particular SKUID
+        /// </summary>
+        public int Units
+        {
+            get { return units; }
+            set { units = value; }
+        }
+    }
+}
